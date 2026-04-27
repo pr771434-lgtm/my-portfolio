5f068129c389ffdff166a36080d1e123533fed49
Pawan Rajpoot <pr771434@gmail.com>
2026-04-27 16:14:55 +0530

    Fix: remove unused AnimatePresence to pass Vercel build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import Particles, { initParticlesEngine } from "@tsparticles/react";
 import { loadSlim } from "@tsparticles/slim";
 import { FiSearch, FiMic } from 'react-icons/fi'; 
